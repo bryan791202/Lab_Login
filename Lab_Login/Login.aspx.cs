916b65ad13cef54a5a0bf12324712e45e4485f3f
bryan791202 <bryan791202psc@gmail.com>
2017-06-20 23:57:23 +0800

    Add files via upload

---
+++ Lab_Login/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (txtUserName.Text == "")
+        {
+            lblPrompt.Text = "Keyin your name";
+            return;
+        }
+        Session["userName"] = txtUserName.Text;
+
+        if (IsPostBack == true)
+        {
+            if (Session["username"] == null)
+            {
+                Response.Redirect("Defaule.aspx");
+            }
+            else
+                Response.Redirect("Secret.aspx");
+        }
+
+
+    }
+}
